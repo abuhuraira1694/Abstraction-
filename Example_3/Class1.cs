cadf7aa70e768439771a33e2b9cbb47be3466f61
Abu-Huraira <abuhuraira1694@gmail.com>
2025-12-27 21:14:27 +0500

    Example_3 of abstraction

---
+++ Example_3/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_3
+{
+    public class zoo:bird,animal
+    {
+        public void eat()
+        {
+            Console.WriteLine("all animals and birds eat");
+        }
+        public void sleep()
+        {
+            Console.WriteLine("all animals and birds sleep");
+        }
+        public void move()
+        {
+            Console.WriteLine("animals move on earth and birds can fly in the air");
+        }
+        public void sound()
+        {
+            Console.WriteLine("all animals and birds have different sound according to their species");
+        }
+    }
+}
---
+++ Example_3/Interface1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_3
+{
+    public interface animal
+    {
+        void eat();
+        void sleep();
+        void sound();
+        void move();
+    }
+}
